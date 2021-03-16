e8812db6d159bb941eff22ee2d552abfa4f1155e
Igor Petrov <letniy71@gmail.com>
2021-03-16 12:14:10 +0300

    main

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,11 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+use App\Models\Brigade;
+use App\Models\Role;
 
 class RegisterController extends Controller
 {
@@ -38,7 +43,8 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        //$this->middleware('guest');
+        $this->middleware('admin');
     }
 
     /**
@@ -47,6 +53,8 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+
+
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -64,14 +72,25 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $brigade = Brigade::where('active',1)
+                        ->where('nameBrigade',$data['nameBrigade'])
+                        ->first();
+
+
+        $role = Role::where('name',$data['role'])
+                        ->first();
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'login'=> $data['login'],
+            'idBrigade'=> $brigade->idBrigade,
+            'idRole'=> $role->idRole,
         ]);
     }
 
-        //Получаем список пользователей
+    //Получаем список пользователей
     public function getUsers()
     {
         $users = User::where('active', 1)
@@ -90,6 +109,7 @@ class RegisterController extends Controller
               ->delete();
     return redirect()->route('register');
   }
+
 
 //Передаем данные для редактирования на страницу редакирования
 public function showEditUser(Request $request){
@@ -114,4 +134,5 @@ public function editUser(Request $request){
     
      return redirect()->route('register');
   }
+ 
 }
